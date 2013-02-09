ac957c6e6ac9d5a3ed5b3650eba265c4175dd2d6
heartcrazed <katharina.franz@gmail.com>
2013-02-09 19:33:43 +0100

    updated style and content

--- v2/index.php
+++ v2/index.php
@@ -12,56 +12,33 @@
 
 <body>
 	
-	<div id="inhalt">
+	<div id="header" class="center">
+		<h1><a href="./">Katharina Franz</a></h1>
+		<h2><a href="http://blog.katharinafranz.com">Blog</a></h2> <span class="ornament">&#9753;</span> 
+		<h2><a href="http://500px.com/heartcrazed">Photography</a></h2> <span class="ornament">&#9753;</span> 
+		<h2><a href="http://heartcrazed.tumblr.com">Tumblr</a></h2>
+	</div>
+	
+	<div id="inhalt">		
 		<div class="center">
-			<h1><a href="./">Katharina Franz</a></h1>
-			<h2><a href="http://kattitu.de">Blog</a></h2> <span class="ornament">&#9753;</span> 
-			<h2><a href="http://500px.com/heartcrazed">Photography</a></h2> <span class="ornament">&#9753;</span> 
-			<h2><a href="http://heartcrazed.tumblr.com">Tumblr</a></h2>
-			
-			<br style="clear:both; margin-bottom:25px;" />
-			
 			<img id="portrait" src="img/katharina.png" alt="this is me." />
 
 				<h3>about me</h3>
 				<p>I’m interested in <strong>web design</strong>, <strong>photography</strong> and <strong>typography</strong> and working with technologies / software like <strong>HTML5</strong>, <strong>CSS3</strong>, <strong>Wordpress</strong>, <strong>Adobe Photoshop</strong>, <strong>InDesign</strong> and <strong>Illustrator</strong>.</p>
 				<h3>current projects</h3>
-				<ul>
+				<ul id="projects">
+					<li><a href="http://kalorienhauptquartier.tumblr.com">kalorienhauptquartier.tumblr.com</a> (01/2013)</li>
+					<li><a href="http://blog.katharinafranz.com">blog.katharinafranz.com</a> (12/2012)</li>
 					<li><a href="http://stadtlandfluss-geschichten.de">stadtlandfluss-geschichten.de</a> (06/2012)</li>
+					<li><a href="http://katharinafranz.com">katharinafranz.com</a> (12/2011)</li>
 					<li><a href="http://typofails.tumblr.com">typofails.tumblr.com</a> (04/2011)</li>
+					<li><del><a href="http://maedchendinge.de">maedchendinge.de</a> (02/2011)</del></li>
+					<li><a href="http://onlinewerk.info">onlinewerk.info</a> (01/2011)</li>
 					<li><a href="http://kattitu.de">kattitu.de</a> (11/2010)</li>
 				</ul>
 			
 			<br style="clear:both;" />
 		</div>
-
-
-		<!--table border="0">
-			<tr>
-				<td class="links"><p><span class="zeile">Projekte</span></p></td>
-				<td class="rechts">
-					<p><span class="zeile"><del><a href="http://maedchendinge.de">mædchendinge.de</a> (02/2011)</del></span></p>
-					<p><span class="zeile"><a href="http://typofails.tumblr.com">typofails.tumblr.com</a> (04/2011)</span></p>
-				</td>
-			</tr>
-			<tr>
-				<td class="links">
-					<p><span class="zeile">Ausbildung</span></p>
-					<p><span class="zeile">(2009&mdash;2012)</span></p></td>
-				<td class="rechts">
-					<p><a href="http://dhbw-mosbach.de/on"><span class="zeile">Duale Hochschule Baden-W&uuml;rttemberg Mosbach</span><br />
-					<span class="zeile">Studiengang Onlinemedien</span></a></p>
-					<p><span class="zeile">Angestrebter Abschluss: Bachelor of Arts</span></p>
-					<p>&nbsp;</p>
-				</td>
-			</tr>
-			<tr>
-				<td class="links"><p><span class="zeile">Social Media</span></p></td>
-				<td class="rechts">
-					<p><span class="zeile"><strong style="color:#3B5998;">facebook.com</strong></a></span> <span class="zeile"><a href="http://www.twitter.com/heartcrazed"><strong style="color:#2299CC;">twitter.com</strong></a></span> <span class="zeile"><a href="http://www.lastfm.de/user/heartcrazed"><strong style="color:#DD1303;">last.fm</strong></a></span> <span class="zeile"><a href="http://heartcrazed.tumblr.com"><strong style="color:#3C5873;">tumblr.com</strong></a></span> <span class="zeile"><a href="http://dribbble.com/heartcrazed"><strong style="color:#C5376D;">dribbble.com</strong></a></span> <span class="zeile"><a href="http://500px.com/heartcrazed"><strong style="color:black;">500px.com</strong></a></span> <span class="zeile"><a href="http://vimeo.com/heartcrazed"><strong style="color:#0DADD6;">vimeo.com</strong></a></span> <span class="zeile"><a href="http://www.youtube.com/user/heartcrzd"><strong style="color:#FF2312;">youtube.com</strong></a></span> <span class="zeile"><a href="http://zootool.com/user/heartcrazed"><strong style="color:#49361F;">zootool.com</strong></a></span></p>
-				</td>
-			</tr>
-		</table-->
 	</div>
 	
 	<div id="social">
@@ -79,7 +56,7 @@
 		<div id="footer">
 		<p><a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#109;&#97;&#105;&#108;&#64;&#107;&#97;&#116;&#104;&#97;&#114;&#105;&#110;&#97;&#102;&#114;&#97;&#110;&#122;&#46;&#99;&#111;&#109;" rel="nofollow">&#109;&#97;&#105;&#108;&#64;&#107;&#97;&#116;&#104;&#97;&#114;&#105;&#110;&#97;&#102;&#114;&#97;&#110;&#122;&#46;&#99;&#111;&#109;</a>
 		<br />
-		<a href="http://www.kattitu.de/impressum/">Impressum</a></p>
+		<a href="http://blog.katharinafranz.com/impressum/">Impressum</a></p>
 	</div>
 
 <!-- GOOGLE ANALYTICS -->
@@ -87,12 +64,13 @@
 
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-11098363-7']);
+  _gaq.push(['_setDomainName', 'katharinafranz.com']);
   _gaq.push(['_trackPageview']);
 
   (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 
 </script>
